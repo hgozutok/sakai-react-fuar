782ebef1b03c5f2ea5fbd613e9e0bd01ab1e6f55
Huseyin GOZUTOK <hgozutok@gmail.com>
2022-04-27 08:11:31 +0300

    .

--- src/service/NodeService.js
+++ src/service/NodeService.js
@@ -5,7 +5,7 @@ export class NodeService {
         return axios.get('assets/demo/data/treenodes.json')
             .then(res => res.data.root);
     }
-
+    
 	getTreeTableNodes() {
 		return axios.get('assets/demo/data/treetablenodes.json')
 			.then(res => res.data.root);
